2bc4af6abfa008e9242ffc050784e87e4976a470
SHappyCoding <48560465+SHappyCoding@users.noreply.github.com>
2021-02-19 21:50:13 +0800

    二叉搜索树结点
    一个值和两个指针，左孩子和右孩子

---
+++ 算法导论/十二章/BST_Node.java
@@ -0,0 +1,11 @@
+public class BST_Node {
+    public Integer data;
+    public BST_Node left, right;
+
+    public BST_Node(Integer data)
+    {
+        this.data = data;
+        //left=right=null;//默认赋值null
+    }
+
+}
