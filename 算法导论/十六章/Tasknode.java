4a78e9f090b1308111c277fd8b03159a2b63d80b
SHappyCoding <48560465+SHappyCoding@users.noreply.github.com>
2021-02-26 13:58:00 +0800

    任务结构
    描述了一个任务的属性和方法：队列中位置，截止时间和惩罚

---
+++ 算法导论/十六章/Tasknode.java
@@ -0,0 +1,12 @@
+public class Tasknode {
+    public int index;
+    public int deadline;
+    public int Wi;
+
+    public Tasknode(int index,int deadline,int Wi)
+    {
+        this.index=index;
+        this.deadline=deadline;
+        this.Wi=Wi;
+    }
+}
