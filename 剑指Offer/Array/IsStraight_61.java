dadc76fcb6cf964ed1629e640e738b72b8930e71
SHappyCoding <48560465+SHappyCoding@users.noreply.github.com>
2021-04-05 23:28:50 +0800

    扑克牌顺子

---
+++ 剑指Offer/Array/IsStraight_61.java
@@ -0,0 +1,35 @@
+public class IsStraight_61 {
+    //set+遍历
+    public boolean isStraight(int[] nums) {
+        Set<Integer> repeat = new HashSet<>();
+        int max = 0, min = 14;
+        for(int num : nums) {
+            if(num == 0) continue; // 跳过大小王
+            max = Math.max(max, num); // 最大牌
+            min = Math.min(min, num); // 最小牌
+            if(repeat.contains(num)) return false; // 若有重复，提前返回 false
+            repeat.add(num); // 添加此牌至 Set
+        }
+        return max - min < 5; // 最大牌 - 最小牌 < 5 则可构成顺子
+    }
+
+    //排序+遍历
+    public boolean isStraight2(int[] nums) {
+        int joker = 0;
+        Arrays.sort(nums); // 数组排序
+        for(int i = 0; i < 4; i++) {
+            if(nums[i] == 0) joker++; // 统计大小王数量
+            else if(nums[i] == nums[i + 1]) return false; // 若有重复，提前返回 false
+        }
+        return nums[4] - nums[joker] < 5; // 最大牌 - 最小牌 < 5 则可构成顺子
+    }
+
+    public static void main(String[] args)
+    {
+        IsStraight_61 sr=new IsStraight_61();
+        int[] a={0,0,1,2,5};
+        System.out.println(sr.isStraight(a));
+        System.out.println(sr.isStraight2(a));
+
+    }
+}
