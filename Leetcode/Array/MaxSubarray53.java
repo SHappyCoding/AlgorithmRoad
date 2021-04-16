f35cba495160914cb94e1f0b643a5ba02ad253e5
SHappyCoding <48560465+SHappyCoding@users.noreply.github.com>
2021-04-16 23:21:19 +0800

    最大子序和

---
+++ Leetcode/Array/MaxSubarray53.java
@@ -0,0 +1,39 @@
+public class MaxSubarray53 {
+    public class Status {
+        public int lSum, rSum, mSum, iSum;
+
+        public Status(int lSum, int rSum, int mSum, int iSum) {
+            this.lSum = lSum;//以l为端点的左边最大子序和
+            this.rSum = rSum;//以r为端点的右边最大子序和
+            this.mSum = mSum;//最大子序和
+            this.iSum = iSum;//区间和
+        }
+    }
+
+    public int maxSubArray(int[] nums) {
+        return getInfo(nums, 0, nums.length - 1).mSum;
+    }
+
+    public Status getInfo(int[] a, int l, int r) {
+        if (l == r) {
+            return new Status(a[l], a[l], a[l], a[l]);
+        }
+        int m = (l + r) >> 1;
+        Status lSub = getInfo(a, l, m);
+        Status rSub = getInfo(a, m + 1, r);
+        return pushUp(lSub, rSub);
+    }
+
+    public Status pushUp(Status l, Status r) {
+        int iSum = l.iSum + r.iSum;
+        int lSum = Math.max(l.lSum, l.iSum + r.lSum);
+        int rSum = Math.max(r.rSum, r.iSum + l.rSum);
+        int mSum = Math.max(Math.max(l.mSum, r.mSum), l.rSum + r.lSum);
+        return new Status(lSum, rSum, mSum, iSum);
+    }
+    public static void main(String[] args) {
+        MaxSubarray53 m4=new MaxSubarray53();
+        int[] a={-2,1,-3,4,-1,2,1,-5,4};
+        System.out.println(m4.maxSubArray(a));
+    }
+}
